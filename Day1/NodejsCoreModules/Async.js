0de319a2385a2f041b1a19682b23d0019328c941
Gaurav10997 <avdhika@gmail.com>
2023-04-25 02:21:26 +0530

    AsyncfsModules

---
+++ Day1/NodejsCoreModules/Async.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+
+fs.writeFile( "Async.txt" , "Learning about async function"  , (err)=>{
+    console.log("file created")
+})
+
+// same thing jusrt include the Asynchronous behaviour of javascript veryy Easy Thing 
